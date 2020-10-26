8fb6eee7c1e90716a2f60a2c6d92ab8fbe853bfd
m.waqaraziz@yahoo.com <m.waqaraziz@yahoo.com>
2020-10-26 21:03:36 +0500

    Props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Props from './Components/Props'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
+    <div >
+      <Props name="Ali"/>  
     </div>
   );
 }
